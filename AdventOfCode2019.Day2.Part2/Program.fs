dd09692e4639035d0959dd03d55dde7e9344527d
Ben Guenter <guenter.ben@gmail.com>
2020-05-08 15:44:24 -0600

    Day 2 solution (#2)
    Co-authored-by: Ben Guenter <bguenter@petrotranz.com>

---
+++ AdventOfCode2019.Day2.Part2/Program.fs
@@ -0,0 +1,36 @@
+﻿open IntcodeComputer
+open Microsoft.FSharp.Core
+open System
+open System.IO
+
+type Operation = Add | Multiply | End
+
+[<EntryPoint>]
+let main argv =
+    let expected = 19690720;
+    let opcodes = File.ReadAllText("input.txt").Split [|','|] |> Seq.map Int32.Parse
+    let beginning = Seq.take 1 opcodes
+    let last = Seq.skip 3 opcodes
+
+    let iterateNounAndVerb noun verb =
+        match (noun, verb) with
+        | (99, 99) -> failwith "No combination found"
+        | (_, 99) -> (noun+1, 0)
+        | (_, _) -> (noun, verb+1)
+
+    let computeNounAndVerb noun verb =
+        let intcodes = Seq.concat [|beginning; Seq.ofList [noun;verb]; last|]
+        IntcodeComputer.Compute intcodes
+
+    let iterateVerbWithNoun (noun, verb) =
+        let result = computeNounAndVerb noun verb
+        if (result = expected) then
+            None
+        else
+            let nextNounAndVerb = (iterateNounAndVerb noun verb)
+            Some (nextNounAndVerb, nextNounAndVerb)
+
+    let result = Seq.unfold iterateVerbWithNoun (0, 0)
+    printf "%A" (Seq.last result)
+
+    0 // return an integer exit code
--- AdventOfCode2019.Day2/Program.fs
+++ AdventOfCode2019.Day2/Program.fs
@@ -1,4 +1,36 @@
-﻿[<EntryPoint>]
+﻿open Microsoft.FSharp.Core
+open System
+open System.Collections.Generic
+open System.IO
+
+type Operation = Add | Multiply | End
+
+[<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    let opcodes = List<int> (File.ReadAllText("input.txt").Split [|','|] |> Seq.map Int32.Parse)
+    let operate currentOpcodeGroupIndex operation =
+        let destinationIndex = opcodes.[currentOpcodeGroupIndex+3]
+        let x = opcodes.[opcodes.[currentOpcodeGroupIndex+1]]
+        let y = opcodes.[opcodes.[currentOpcodeGroupIndex+2]]
+
+        match operation with
+        | Add -> Some (opcodes.[destinationIndex] <- x + y)
+        | Multiply -> Some (opcodes.[destinationIndex] <- x * y)
+        | End -> None
+
+    let iterateAndOperate currentOpcodeGroupIndex =
+        let operation =
+            match opcodes.[currentOpcodeGroupIndex] with
+            | 1 -> Add
+            | 2 -> Multiply
+            | 99 -> End
+            | _ -> failwith (sprintf "Invalid Opcode: '%i'" opcodes.[currentOpcodeGroupIndex])
+        let result = operate (currentOpcodeGroupIndex) operation
+        match result with
+        | Some _ -> Some (opcodes.[0], currentOpcodeGroupIndex + 4)
+        | None -> None
+
+    // Using unfold so that we can end on demand (return of None)
+    Seq.unfold iterateAndOperate 0 |> Seq.toList |> ignore
+    printfn "%i" opcodes.[0]
     0 // return an integer exit code
