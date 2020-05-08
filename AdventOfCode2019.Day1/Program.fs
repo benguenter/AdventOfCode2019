7d5e565e3207c2227632c12204d582cf6016a4ff
Ben Guenter <guenter.ben@gmail.com>
2020-05-08 15:40:37 -0600

    Day 1 Solution (#1)
    Co-authored-by: Ben Guenter <bguenter@petrotranz.com>

--- AdventOfCode2019.Day1/Program.fs
+++ AdventOfCode2019.Day1/Program.fs
@@ -1,4 +1,22 @@
-﻿[<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
+﻿open System
+open System.IO
+
+[<EntryPoint>]
+let main argv =
+    let calcFuel mass = (mass/3) - 2
+    
+    let unfoldFuel fuel =
+        match (calcFuel fuel) with
+        | requiredFuel when requiredFuel > 0 -> Some(requiredFuel, requiredFuel)
+        | _ -> None
+
+    let findRequiredFuel mass = Seq.unfold unfoldFuel mass
+
+    let lines = File.ReadAllLines("input.txt")
+    let total = 
+        lines
+            |> Seq.map (fun l -> Int32.Parse l |> findRequiredFuel)
+            |> Seq.concat
+            |> Seq.sum
+    printfn "%i" total
     0 // return an integer exit code
